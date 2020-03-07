8db5b026d9ab2fbae1a5fb4238938216898b843a
Blake Fogle <foglebd@gmail.com>
2020-03-07 12:55:26 -0800

    ready for testing on Heroku

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://heroku_t3xl531g:legolas12@ds035488.mlab.com:35488/heroku_t3xl531g",
+  mongoURI: "mongodb://heroku_t3xl531g:c0l0r@d0@ds035488.mlab.com:35488/heroku_t3xl531g",
   secretOrKey: "secret"
 };
